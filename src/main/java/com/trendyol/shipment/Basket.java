62cf84928f7f0db4a565803b659f8d55492cb6a9
Ali Berk Bostancı <55848455+berkbstnc@users.noreply.github.com>
2023-10-02 22:38:58 +0300

    istenilen business order imlement edildi

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -1,13 +1,60 @@
 package com.trendyol.shipment;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Basket {
-
     private List<Product> products;
 
     public ShipmentSize getShipmentSize() {
-        return null;
+
+        if (getProducts().size() < 3) {
+            if (getProducts().isEmpty()) {
+                return null;
+            }
+
+            return getProducts().stream()
+                    .map(Product::getSize)
+                    .max(Comparator.naturalOrder())
+                    .orElse(null);
+        } else {
+            Map<ShipmentSize, Long> sizeOfMap = getProducts().stream()
+                    .collect(Collectors.groupingBy(Product::getSize, Collectors.counting()));
+
+            if (sizeOfMap.containsKey(ShipmentSize.X_LARGE) && sizeOfMap.get(ShipmentSize.X_LARGE) >= 3) {
+                return ShipmentSize.X_LARGE;
+            }
+
+            Optional<Map.Entry<ShipmentSize, Long>> numberOfFrequency = sizeOfMap.entrySet().stream()
+                    .max(Comparator.comparing(Map.Entry::getValue));
+
+            if (numberOfFrequency.isPresent()) {
+                Map.Entry<ShipmentSize, Long> size = numberOfFrequency.get();
+                if (size.getValue() >= 3) {
+                    if (size.getKey() == ShipmentSize.SMALL) {
+                        return ShipmentSize.MEDIUM;
+                    } else if (size.getKey() == ShipmentSize.MEDIUM) {
+                        return ShipmentSize.LARGE;
+                    } else if (size.getKey() == ShipmentSize.LARGE) {
+                        return ShipmentSize.X_LARGE;
+                    } else {
+                        return null;
+                    }
+                }else{
+                    if (getProducts().isEmpty()) {
+                        return null;
+                    }
+
+                    return getProducts().stream()
+                            .map(Product::getSize)
+                            .max(Comparator.naturalOrder())
+                            .orElse(null);
+                }
+
+            } else {
+                return null;
+            }
+        }
     }
 
     public List<Product> getProducts() {
